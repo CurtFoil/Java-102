7de77ee92892c12e94c1a74e3301e44e95c9cdcf
CurtFoil <zihel@nycstudents.net>
2024-11-24 18:43:07 -0500

    Start of Java102

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    
     public static void main(String[] args) {
-        
+        Point point = new Point(4, 3);   
+        Point xTranslation = point.translateX(19);
+        Point yTranslation = point.translateY(-56);
+        Point xyTranslation = point.translateX(84).translateY(-34);System.out.println("x transl: (" + xTranslation.x + ", " + xTranslation.y + ")");System.out.println("y transl: (" + yTranslation.x + ", " + yTranslation.y + ")");System.out.println("xy transl: (" + xyTranslation.x + ", " + xyTranslation.y + ")");
     }
 }
