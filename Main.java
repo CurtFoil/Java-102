6a8b60190bd52eb66be5eb5fbabeeb77d0fe9a49
CurtFoil <zihel@nycstudents.net>
2024-11-29 17:41:38 -0500

    Going though java 102

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 public class Main {
     
     public static void main(String[] args) {
-        Point point = new Point(4, 3);   
+        Point point = new Point(4, 3);
         Point xTranslation = point.translateX(19);
         Point yTranslation = point.translateY(-56);
-        Point xyTranslation = point.translateX(84).translateY(-34);System.out.println("x transl: (" + xTranslation.x + ", " + xTranslation.y + ")");System.out.println("y transl: (" + yTranslation.x + ", " + yTranslation.y + ")");System.out.println("xy transl: (" + xyTranslation.x + ", " + xyTranslation.y + ")");
+        Point xyTranslation = point.translateX(84).translateY(-34);
+        
+        // System.out.println("x transl: (" + xTranslation.x + ", " + xTranslation.y + ")");
+        // System.out.println("y transl: (" + yTranslation.x + ", " + yTranslation.y + ")");
+        // System.out.println("xy transl: (" + xyTranslation.x + ", " + xyTranslation.y + ")");
+
+        System.out.println("point: " + point);
+        System.out.println("x transl: " + xTranslation);
+        System.out.println("y transl: " + yTranslation);
+        System.out.println("xy transl: " + xyTranslation);
+
+        Point a = new Point (4.5, 3.9);
+        Point b = new Point (7.2, 5.5);
+        System.out.println(Point.distance(a, b));
+
+        Point[] points = { a, b };
+        System.out.println(Point.centerOfMass(points));
     }
 }
--- Point.java
+++ Point.java
@@ -14,4 +14,29 @@ public class Point {
 	public Point translateY(double t) {
 		return new Point(x, y + t);
 	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+	}
+
+	public static Point centerOfMass(Point[] points){
+		double addx = 0;
+		double addy = 0;
+
+		for (Point point : points) {
+			addx += point.x;
+			addy += point.y;
+		}
+
+		double centerx = addx / points.length;
+		double centery = addy / points.length;
+
+		return new Point(centerx, centery);
+	}
+
 }
